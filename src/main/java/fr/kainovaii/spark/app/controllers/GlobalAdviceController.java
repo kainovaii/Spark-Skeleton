c9716fe5f97f762b300d0b66ff82b167addb50d8
Your Name <52110617+kainovaii@users.noreply.github.com>
2026-02-10 17:48:14 +0100

    Dev (Remove default global variable )

--- src/main/java/fr/kainovaii/spark/app/controllers/GlobalAdviceController.java
+++ src/main/java/fr/kainovaii/spark/app/controllers/GlobalAdviceController.java
@@ -2,6 +2,7 @@ package fr.kainovaii.spark.app.controllers;
 
 import fr.kainovaii.core.web.controller.BaseController;
 import spark.Request;
+import spark.Response;
 
 import java.util.Map;
 
@@ -9,13 +10,8 @@ import static fr.kainovaii.core.web.template.TemplateManager.setGlobal;
 
 public class GlobalAdviceController extends BaseController
 {
-
-    public static void applyGlobals(Request req)
+    public static void applyGlobals(Request req, Response res)
     {
-        setGlobal("isLogged", isLogged(req));
-        if (isLogged(req)) setGlobal("loggedUser", getLoggedUser(req));
-        Map<String, String> flashes = collectFlashes(req);
-        setGlobal("flashes", flashes);
-        setGlobal("request", req);
+
     }
 }
