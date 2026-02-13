a6fb0982ddb1d4058e2ccd2275f9c57c7b985702
Your Name <52110617+kainovaii@users.noreply.github.com>
2026-02-13 23:43:18 +0100

    Dev (Update doc and pom.xml)

--- src/main/java/fr/kainovaii/obsidian/Main.java
+++ src/main/java/fr/kainovaii/obsidian/Main.java
@@ -1,13 +1,11 @@
 package fr.kainovaii.obsidian;
 
-
 import fr.kainovaii.obsidian.core.Obsidian;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Obsidian app = new Obsidian();
-        app.init();
+        Obsidian.run(Main.class);
     }
 }
--- src/main/java/fr/kainovaii/obsidian/app/controllers/GlobalAdviceController.java
+++ src/main/java/fr/kainovaii/obsidian/app/controllers/GlobalAdviceController.java
@@ -1,10 +1,11 @@
 package fr.kainovaii.obsidian.app.controllers;
 
-
 import fr.kainovaii.obsidian.core.web.controller.BaseController;
+import fr.kainovaii.obsidian.core.web.controller.GlobalAdvice;
 import spark.Request;
 import spark.Response;
 
+@GlobalAdvice
 public class GlobalAdviceController extends BaseController
 {
     public static void applyGlobals(Request req, Response res)
